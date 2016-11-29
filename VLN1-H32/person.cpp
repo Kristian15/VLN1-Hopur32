7f400e30c6463232eae08c0f066521a8362d89f2
Berglind Sigurdardottir <Berglind Sigurdardottir>
2016-11-29 13:43:02 +0000

    layout breytingar í Person

--- VLN1-H32/person.cpp
+++ VLN1-H32/person.cpp
@@ -1,10 +1,10 @@
 #pragma once
 #include "person.h"
-using namespace std;
-Person::Person()
-{
 
-}
+using namespace std;
+
+Person::Person(){}
+
 Person::Person(string name, string gender, int byear, int dyear)
 {
     _name = name;
@@ -13,40 +13,49 @@ Person::Person(string name, string gender, int byear, int dyear)
     _dyear  = dyear;
 
 }
+
 Person::Person(string name, string gender, int byear)
 {
     _name   = name;
     _gender = gender;
     _byear  = byear;
 }
+
 string Person:: getName()
 {
     return _name;
 }
+
 string Person:: getGender()
 {
     return _gender;
 }
+
 int Person::getByear()
 {
     return _byear;
 }
+
 int Person:: getDyear()
 {
     return _dyear;
 }
+
 void Person:: setName(string name)
 {
     _name = name;
 }
+
 void Person:: setGender(string gender)
 {
     _gender = gender;
 }
+
 void Person:: setByear(int byear)
 {
     _byear = byear;
 }
+
 void Person:: setDyear(int dyear)
 {
     _dyear = dyear;
