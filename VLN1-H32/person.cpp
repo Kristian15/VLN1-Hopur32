12247bf0f93058119dace0e135cbac897c808fb3
Kristian Laursen <kristian15@ru.is>
2016-11-30 13:16:19 +0000

    Nationality added to datalayer and person class

--- VLN1-H32/person.cpp
+++ VLN1-H32/person.cpp
@@ -5,67 +5,30 @@ using namespace std;
 
 Person::Person(){}
 
-Person::Person(string name, string gender, int byear, int dyear)
+Person::Person(string name, string gender, string nationality, int byear, int dyear)
 {
     _name = name;
     _gender = gender;
+    _nationality = nationality;
     _byear= byear;
     _dyear  = dyear;
 
 }
 
-Person::Person(string name, string gender, int byear)
+Person::Person(string name, string gender, string nationality, int byear)
 {
     _name   = name;
     _gender = gender;
+    _nationality = nationality;
     _byear  = byear;
     _dyear  = 0;
-}
-
-string Person:: getName()
-{
-    return _name;
-}
-
-string Person:: getGender()
-{
-    return _gender;
-}
-
-int Person::getByear()
-{
-    return _byear;
-}
-
-int Person:: getDyear()
-{
-    return _dyear;
-}
-
-void Person:: setName(string name)
-{
-    _name = name;
-}
-
-void Person:: setGender(string gender)
-{
-    _gender = gender;
-}
-
-void Person:: setByear(int byear)
-{
-    _byear = byear;
-}
-
-void Person:: setDyear(int dyear)
-{
-    _dyear = dyear;
 }
 
 ostream& operator <<(ostream& lhs, Person& rhs)
 {
     lhs << "Name:          " << rhs.getName() << endl;
     lhs << "Gender:        " << rhs.getGender() << endl;
+    lhs << "Nationality:   " << rhs.getNationality();
     lhs << "Year of birth: " << rhs.getByear() << endl;
 
     if(rhs.getDyear() > 0)
