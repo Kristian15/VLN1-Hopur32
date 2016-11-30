deb5939ed1bc2b3c2a9038b17e22d2201b2f3d49
Kristian Laursen <kristian15@ru.is>
2016-11-30 15:36:09 +0000

    Datalayer additions

--- VLN1-H32/datalayer.cpp
+++ VLN1-H32/datalayer.cpp
@@ -5,12 +5,72 @@ dataLayer::dataLayer()
 
 }
 
+vector<Person> dataLayer::loadDataP(string fileName)
+{
+    string line;
+    vector<Person> persons;
+
+    _fileName = fileName;
+    data.clear();
+
+    ifstream iDataStream;
+    iDataStream.open(fileName);
+
+    if(iDataStream)
+    {
+        while(getline(iDataStream,line))
+        {
+            data.push_back(line);
+        }
+
+        iDataStream.close();
+    }
+
+    for (unsigned int i = 0; i < data.size(); i++)
+    {
+        string s = data[i];
+        vector<string> elems;
+        string delimeter = ";";
+        size_t pos = 0;
+        string token;
+
+        while ((pos = s.find(delimeter)) != string::npos)
+        {
+            token = s.substr(0, pos);
+            elems.push_back(token);
+            s.erase(0, pos + delimeter.length());
+        }
+
+        string Name, Gender, Nationality, Byear, Dyear;
+
+        Name = elems[0];
+        Gender = elems[1];
+        Nationality = elems[2];
+        Byear = elems[3];
+
+        if(elems.size() == 5)
+        {
+            Dyear = elems[4];
+            Person newPerson= Person(Name, Gender, Nationality, stoi(Byear), stoi(Dyear));
+            persons.push_back(newPerson);
+        }
+
+        else
+        {
+            Person newPerson = Person(Name, Gender, Nationality, stoi(Byear));
+            persons.push_back(newPerson);
+        }
+    }
+
+    return persons;
+}
+
 vector<string> dataLayer::loadData(string fileName)
 {
-    data.clear();
     string line;
 
     _fileName = fileName;
+    data.clear();
 
     ifstream iDataStream;
     iDataStream.open(fileName);
@@ -30,8 +90,10 @@ vector<string> dataLayer::loadData(string fileName)
 
 void dataLayer::saveData(vector<Person> persons, string fileName)
 {
-    data.clear();
     string name = "", gender = "", nationality = "", bYear = "", dYear = "", delimiter = ";";
+
+    _fileName = fileName;
+    data.clear();
 
     for (unsigned int i = 0; i < persons.size(); i++)
     {
--- VLN1-H32/datalayer.h
+++ VLN1-H32/datalayer.h
@@ -15,6 +15,7 @@ private:
 public:
     dataLayer();
     // skilar línunum í fileName sem vektor af strengjum
+    vector<Person> loadDataP(string fileName);
     vector<string> loadData(string fileName); // hlaða inn gögnum inn í forritið við ræsingu
     void saveData(vector<Person> persons, string fileName); // vista gögn
     void updateFile(string name, string gender, string nationality, string byear, string dyear); // update dataFile
