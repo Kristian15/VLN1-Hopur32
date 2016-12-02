0b77b2676ee8c98cf03930c8b8888d3b01c4f0bf
Berglind Sigurdardottir <Berglind Sigurdardottir>
2016-12-02 15:38:20 +0000

    .

--- VLN1-H32/servicelayer.cpp
+++ VLN1-H32/servicelayer.cpp
@@ -588,5 +588,5 @@ void serviceLayer::saveData(string fileName, vector<Person> saveMe)
 void serviceLayer::deletePerson(string name)
 {
     data.setPersons(findByNameReverse(name));
-    saveData("current", data.getPersons());
+    saveData("gaur.txt", data.getPersons());
 }
